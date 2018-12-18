8d104ab8317518ab79145b6475d20968e164316e
Sarah Glavan <s.glavan@sarahglavan-mac.local>
2018-12-18 09:27:49 +0100

    init Hello World

---
+++ Assets/MyGame/Scripts/ChangeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeScript : MonoBehaviour {
+
+
+    public Text overrideText;
+
+	// Use this for initialization
+	void Start () {
+
+        overrideText.text = "lalala";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
